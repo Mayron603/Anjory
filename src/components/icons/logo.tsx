d306c030a965cc5f6b4160d80e856af985ba677d
Mayron <mayronpaula9@gmail.com>
2025-11-01 15:58:31 +0000

    consegue aumentar o tamanho da logo?

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 export function Logo(props: React.ComponentProps<'div'>) {
   return (
     <div
-      style={{ width: '150px', height: 'auto' }}
+      style={{ width: '200px', height: 'auto' }}
       {...props}
     >
       <Image
         src="/LOGO_panda.png"
         alt="Anjory Logo"
-        width={150}
-        height={30}
+        width={200}
+        height={40}
         priority
       />
     </div>
